48c3088fbbf224833f8684209654a7f56d629642
Yato97 <global>
2021-04-07 22:03:41 +0400

    Déplacement

--- app/src/main/java/com/yato/projet/Activity0.java
+++ app/src/main/java/com/yato/projet/Activity0.java
@@ -3,11 +3,15 @@ package com.yato.projet;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,22 +19,79 @@ public class Activity0 extends AppCompatActivity {
     //-----------------------Layout--------------------------//
     private TextView score;
     private ImageView perso;
-    private ImageView button;
+    private Button button, buttong, buttond , buttona, buttonb;
+
     //-----------------------Layout--------------------------//
 
 
+    //-------------------------Control------------------------//
+    private boolean touchControl = false;
+    private boolean startControl = false;
 
+    private Runnable runnable;
+    private Handler handler;
+    private String action;
+    //-------------------------Control------------------------//
+
+
+    //-------------------------Camera------------------------//
+    private FrameLayout frameLayout;
+    int posX; //Positions
+    int posY;
+
+    int screenWidth; //Dimentions ecran
+    int screenHeight;
+    //-------------------------Camera------------------------//
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_0);
 
+        //-------------------------Camera------------------------//
+        frameLayout = findViewById(R.id.frame0);
+        //-------------------------Camera------------------------//
+
+
         //-----------------------Layout--------------------------//
         button = findViewById(R.id.button);
+        buttong = findViewById(R.id.buttonG);
+        buttond = findViewById(R.id.buttonD);
+        buttona = findViewById(R.id.buttonA);
+        buttonb = findViewById(R.id.buttonB);
+
+
         score = findViewById(R.id.score);
-        perso = findViewById(R.id.perso);
+        perso = findViewById(R.id.player);
         //-----------------------Layout--------------------------//
 
+
+
+
+        frameLayout.setOnTouchListener((View.OnTouchListener) (v, event) -> {
+            if (!startControl) { //1er click
+                startControl = true;
+
+                //-----------------------Viewport-----------------------//
+                screenWidth = (int)frameLayout.getWidth();
+                screenHeight = (int)frameLayout.getHeight();
+                //-----------------------Viewport-----------------------//
+
+                //-----------------------Playerpos----------------------//
+                posX = (int) perso.getX();
+                posY = (int) perso.getY();
+                //-----------------------Playerpos----------------------//
+
+                handler = new Handler();
+                runnable = () -> {
+                    seDeplace();
+                    handler.postDelayed(runnable,20);
+                };
+                handler.post(runnable);
+            }
+            return true;
+        });
         //-----------------------HOMELISTENER-----------------------//
         //----------------------------------------------------------//
         button.setOnClickListener(new View.OnClickListener() {
@@ -42,7 +103,82 @@ public class Activity0 extends AppCompatActivity {
                 finish();
             }
         });
-        //----------------------------------------------------------//
+        //-----------------------------------------------------------//
         // -----------------------HOMELISTENER-----------------------//
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    public void seDeplace() {
+        //---------------------------Control------------------------//
+        buttond.setOnTouchListener((v, event) -> {
+            action = "AVANCE";
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                touchControl = true;
+            }
+            if (event.getAction() == MotionEvent.ACTION_UP) {
+                touchControl = false;
+            }
+            return false;
+        });
+        buttong.setOnTouchListener((v, event) -> {
+            action = "RECULE";
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                touchControl = true;
+            }
+            if (event.getAction() == MotionEvent.ACTION_UP) {
+                touchControl = false;
+            }
+            return false;
+        });
+        buttona.setOnTouchListener((v, event) -> {
+            action = "SAUTE";
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                touchControl = true;
+            }
+            if (event.getAction() == MotionEvent.ACTION_UP) {
+                touchControl = false;
+            }
+            return false;
+        });
+
+        mouve(action,touchControl);
+        //---------------------------Control------------------------//
+
+        //---------------------------Limites------------------------//
+        if (posY <= 0) {
+            posY = 0;
+        }
+        if (posX <= 0) {
+            posX = 0;
+        }
+        if (posY >= (screenHeight - perso.getHeight())) {
+            posY = (screenHeight - perso.getHeight());
+        }
+        if (posX >= (screenWidth - perso.getWidth())) {
+            posX = (screenWidth - perso.getWidth());
+        }
+        //---------------------------Limites------------------------//
+
+        perso.setY(posY); //Update
+        perso.setX(posX);
+    }
+
+    public void mouve(String action, boolean etat) {
+        if (action == "AVANCE") {
+            if (etat) {
+                posX += (screenWidth / 130);
+            }
+        }
+        if (action == "RECULE") {
+            if (etat) {
+                posX -= (screenWidth / 130);
+            }
+        }
+        if (action == "SAUTE") {
+            if (etat) {
+                posY -= (screenHeight / 50);
+            }
+            else posY += (screenHeight / 50);
+        }
     }
 }
--- app/src/main/java/com/yato/projet/Activity1.java
+++ app/src/main/java/com/yato/projet/Activity1.java
@@ -1,10 +1,17 @@
 package com.yato.projet;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,21 +19,89 @@ public class Activity1 extends AppCompatActivity {
     //-----------------------Layout--------------------------//
     private TextView score;
     private ImageView perso;
-    private ImageView button;
+    private ImageView button, buttong, buttond, buttona, buttonb;
+
     //-----------------------Layout--------------------------//
 
 
+    //-------------------------Control------------------------//
+    private boolean touchControl = false;
+    private boolean startControl = false;
 
+    private Runnable runnable;
+    private Handler handler;
+    //-------------------------Control------------------------//
+
+
+    //-------------------------Camera------------------------//
+    private FrameLayout frameLayout;
+    int posX; //Positions
+    int posY;
+
+    int screenWidth; //Dimentions ecran
+    int screenHeight;
+    //-------------------------Camera------------------------//
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
 
+        //-------------------------Camera------------------------//
+        frameLayout = findViewById(R.id.frame0);
+        //-------------------------Camera------------------------//
+
+
         //-----------------------Layout--------------------------//
         button = findViewById(R.id.button);
+        buttong = findViewById(R.id.buttonG);
+        buttond = findViewById(R.id.buttonD);
+        buttona = findViewById(R.id.buttonA);
+        buttonb = findViewById(R.id.buttonB);
+
+
         score = findViewById(R.id.score);
-        perso = findViewById(R.id.perso);
+        perso = findViewById(R.id.player);
         //-----------------------Layout--------------------------//
+
+
+        frameLayout.setOnTouchListener((View.OnTouchListener) (v, event) -> {
+            if (!startControl) { //1er click
+                startControl = true;
+
+                //-----------------------Viewport-----------------------//
+                screenWidth = (int)frameLayout.getWidth();
+                screenHeight = (int)frameLayout.getHeight();
+                //-----------------------Viewport-----------------------//
+
+                //-----------------------Playerpos----------------------//
+                posX = (int) perso.getX();
+                posY = (int) perso.getY();
+                //-----------------------Playerpos----------------------//
+
+                handler = new Handler();
+                runnable = () -> {
+                    seDeplace();
+                    handler.postDelayed(runnable,20);
+                };
+                handler.post(runnable);
+            }
+
+            else  {
+                //Relachement du click
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    touchControl = true;
+                }
+                //Au click
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    touchControl = false;
+                }
+            }
+            return true;
+        });
+
+
 
         //-----------------------HOMELISTENER-----------------------//
         //----------------------------------------------------------//
@@ -34,12 +109,34 @@ public class Activity1 extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
-                intent.putExtra("score", 140);
-                setResult(1, intent);
+                intent.putExtra("score", 100);
+                setResult(0, intent);
                 finish();
             }
         });
-        //----------------------------------------------------------//
+        //-----------------------------------------------------------//
         // -----------------------HOMELISTENER-----------------------//
+    }
+
+    public void seDeplace() {
+        //---------------------------Control------------------------//
+        if (touchControl) {
+            posY -= (screenHeight / 50);
+        }
+        else {
+            posY += (screenHeight / 50);
+        }
+        //---------------------------Control------------------------//
+
+        //---------------------------Limites------------------------//
+        if (posY <= 0) {
+            posY = 0;
+        }
+        if (posY >= (screenHeight - perso.getHeight())) {
+            posY = (screenHeight - perso.getHeight());
+        }
+        //---------------------------Limites------------------------//
+
+        perso.setY(posY); //Update
     }
 }
